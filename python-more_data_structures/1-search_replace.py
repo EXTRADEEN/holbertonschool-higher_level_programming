3823fe1c2041db0d33f159e33a7d20196702122c
EXTRADEEN <dinumira@gmail.com>
2023-01-24 15:37:06 +0100

    fixing correct output case

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,8 +2,8 @@
 def search_replace(my_list, search, replace):
     new_list = []
 
-    for i in range(1, len(my_list)):
-        if i == search:
+    for i in range(len(my_list)):
+        if my_list[i] == search:
             new_list.append(replace)
         else:
             new_list.append(my_list[i])
