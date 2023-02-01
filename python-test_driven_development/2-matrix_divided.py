4e1f3703d82afb099ec290d9ecab58df715a4263
EXTRADEEN <dinumira@gmail.com>
2023-02-01 12:51:44 +0100

    PEP8 validation fixed

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -16,19 +16,19 @@ def matrix_divided(matrix, div):
             if type(data) is not int and type(data) is not float:
                 raise TypeError("matrix must be a matrix "
                                 "(list of lists) of "
-                                "integers/floats")
-                                
+                                "integers/floats"
+
     row_length = len(matrix[0])
     for row in matrix:
         if len(row) != row_length:
-           raise TypeError("Each row of the matrix "
-                            "must have the same size")
-                    
+                raise TypeError("Each row of the matrix "
+                                "must have the same size")
+
     if type(div) is not int and type(div) is not float:
-       raise TypeError("div must be a number")
+        raise TypeError("div must be a number")
 
     if div == 0:
-       raise ZeroDivisionError("division by zero")
+        raise ZeroDivisionError("division by zero")
 
     new_matrix = [[round(a / div, 2) for a in row] for row in matrix]
 
