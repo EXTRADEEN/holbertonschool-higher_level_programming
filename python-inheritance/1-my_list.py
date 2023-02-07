54d2f46d2294f2c5884919b5e7cf831336ab60b0
EXTRADEEN <dinumira@gmail.com>
2023-02-07 16:45:17 +0100

    a class MyList that inherits from list and hat prints the list, but sorted (ascending sort)

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Module to create inherited class """
+
+
+class MyList(list):
+    """ Creates a new class, MyList thet inherits from list """
+
+    def print_sorted(self):
+        """ prints the list, sorted in ascendig sort """
+        print(sorted(self))
+        return sorted(self)
+    
