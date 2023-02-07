2e9f63c9b795d76cc75a08b5dd906b4d363576aa
EXTRADEEN <dinumira@gmail.com>
2023-02-07 12:50:52 +0100

    function that returns attributes and methods of an object, and README.md file

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    This module checks available attributes
+    and methods of an object
+"""
+
+
+def lookup(obj):
+    """ Returns the attributes and methods """
+
+    return dir(obj)
