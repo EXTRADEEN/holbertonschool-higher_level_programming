32382176a8df1a7164a304294baa3a3c171498bd
EXTRADEEN <dinumira@gmail.com>
2023-02-08 13:53:03 +0100

    function that define a Public instance method: def area(self)

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    Module that creates a class: BeseGeometry
+"""
+
+
+class BaseGeometry:
+    """ Defines the class: BaseGeometry """
+
+    def area(self):
+        """ returns the area """
+
+        raise Exception("area() is not implemented")
