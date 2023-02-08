bf9680ee20fb4f046bda1e12e14f6cb7f33a561d
EXTRADEEN <dinumira@gmail.com>
2023-02-08 15:41:22 +0100

    function that creates Public instance method: def integer_validator(self, name, value): that validates value

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    Module that creates a class: BeseGeometry
+"""
+
+
+class BaseGeometry:
+    """ Defines the class: BaseGeometry """
+
+    def area(self):
+        """ returns the area """
+
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """ validates value """
+
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
+        
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")
