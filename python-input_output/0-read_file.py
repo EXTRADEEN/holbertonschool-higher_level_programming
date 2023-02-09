38ee7ba71cfc1561aeab366d44f3c2f91b57312e
EXTRADEEN <dinumira@gmail.com>
2023-02-09 12:21:50 +0100

    function that reads a text file and prints it to stdout

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    Module that creates a function: read_file
+"""
+
+
+def read_file(filename=""):
+    """ Function that reads a text file (UTF8) and prints it to stdout """
+
+    with open(filename, encoding="utf-8") as file:
+        print(file.read())
