aad21d2fbf5275a05d3242884d27d53270627052
EXTRADEEN <dinumira@gmail.com>
2023-02-13 15:24:30 +0100

    PEB8 validation fixed

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -98,5 +98,5 @@ class Rectangle(Base):
     def __str__(self):
         """ Returns the string representation """
 
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y,
-                                                self.__width, self.__height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format
+    (self.id, self.__x, self.__y, self.__width, self.__height)
