2f400ec1ab91e6fb19d79b7e2d09380974534fc1
EXTRADEEN <dinumira@gmail.com>
2023-02-13 15:21:41 +0100

    updated Rectangle, returns string representation of Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -94,3 +94,9 @@ class Rectangle(Base):
             for j in range(self.__width):
                 print('#', end="")
             print()
+
+    def __str__(self):
+        """ Returns the string representation """
+
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y,
+                                                self.__width, self.__height)
