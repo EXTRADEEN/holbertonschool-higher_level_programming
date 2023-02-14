77e9c8d6a54176f43a33cdfadad3f37fa2f44ce1
EXTRADEEN <dinumira@gmail.com>
2023-02-14 10:55:14 +0100

    the new class Square that inherits from Rectangle

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    MOdule thet creates a class: Square
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ Defines a new class Square """
+
+    def __init__(self, size, x=0, y=0, id=None):
+
+        self.size = size
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
