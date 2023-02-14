3c4c84f5ef659c3f38adab0e6098aa6810cba1c4
EXTRADEEN <dinumira@gmail.com>
2023-02-14 12:08:32 +0100

    added attributes *args and **kwargs

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -25,3 +25,35 @@ class Square(Rectangle):
     def size(self, size):
         self.width = size
         self.height = size
+
+    def update(self, *args, **kwargs):
+        """Update the Square
+        Args:
+            *args (ints): New attribute values
+                - 1st argument represents id attribute
+                - 2nd argument represents size attribute
+                - 3rd argument represents x attribute
+                - 4th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes
+        """
+
+        if args:
+            for i, v in enumerate(args):
+                if i == 1:
+                    self.id = v
+                if i == 2:
+                    self.size = v
+                if i == 3:
+                    self.x = v
+                if i == 4:
+                    self.y = v
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "size":
+                    self.size = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
