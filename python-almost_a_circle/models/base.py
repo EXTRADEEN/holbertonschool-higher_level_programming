ad1d372a9d1bab7f2ef4fb0e6cae91a370a06a2a
EXTRADEEN <dinumira@gmail.com>
2023-02-15 09:36:21 +0100

    output case fixed

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -50,10 +50,9 @@ class Base:
     def create(cls, **dictionary):
         """ returns an instance with all attributes already set """
 
-        if cls.__name__ == 'Rectangle':
+        if cls.__name__ == "Rectangle":
             dummy = cls(1, 1)
-        elif cls.__name__ == 'Square':
-            dummy = cls(1)
         else:
-            dummy.update(**dictionary)
+            dummy = cls(1)
+        dummy.update(**dictionary)
         return dummy
