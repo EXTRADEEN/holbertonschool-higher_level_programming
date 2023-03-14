fce8a7257c8b3b90870dedb5508fcdc46975923f
EXTRADEEN <dinumira@gmail.com>
2023-03-14 14:18:05 +0100

    script that creates an user and grant all privileges

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- creates a new user 'user_0d_1'
 -- the password is set to 'user_0d_1_pwd'
 -- grant all privileges to user_0d_1
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT SELECT ON *.* TO user_0d_1@localhost;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
