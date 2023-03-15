c1c7dbb42ec5b7da7cfbcbe4ab0efed1afad9a0c
EXTRADEEN <dinumira@gmail.com>
2023-03-15 18:18:48 +0100

    same changes

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -2,9 +2,10 @@
 -- create a table 'states' in this database
 -- description id=INT unique, auto generated, can’t be null and is a primary key
 -- descrioption name=VARCHAR(256) can’t be null
-
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa
+
+USE hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS states (
     id INT UNIQUE AUTO_INCREMENT NOT NULL,
     name VARCHAR(256) NOT NULL,
