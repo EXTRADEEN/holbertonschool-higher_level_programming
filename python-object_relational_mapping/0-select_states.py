9f4edfbd68f34919e5711508b8738ea8d1047536
EXTRADEEN <dinumira@gmail.com>
2023-03-17 11:06:01 +0100

    script that lists all states from the database hbtn_0e_0_usa

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
+''' script that lists all states from the database hbtn_0e_0_usa '''
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    db_name = sys.argv[3]
 
     ''' connect to MySQL server running on localhosh at port 3306 '''
     
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=db_name
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
 
     ''' execute SQL query to retrieve all states '''
