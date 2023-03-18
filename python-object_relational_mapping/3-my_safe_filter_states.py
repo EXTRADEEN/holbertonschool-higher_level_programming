a1c68a4b657769b7afb9fc502d1dff2e9cae54da
EXTRADEEN <dinumira@gmail.com>
2023-03-18 23:31:37 +0100

    script that lists all states from the database hbtn_0e_0_usa

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name = %s "
                 "ORDER BY states.id ASC", (state_name_searched, ))
-    
+
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
