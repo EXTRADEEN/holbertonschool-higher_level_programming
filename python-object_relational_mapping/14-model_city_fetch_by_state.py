ad88d7ffb957b6fc876a81a20bc24d77a527c29a
EXTRADEEN <dinumira@gmail.com>
2023-03-19 21:00:18 +0100

    PIP8 validation fixed

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
 
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-                           
+
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     for city, state in session.query(City, State)\
             .filter(City.state_id == State.id)\
             .order_by(City.id):
-        
+
         print("{}: ({}) {}".format(state.name, city.id, city.name))
 
     session.close()
