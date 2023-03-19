2c1d48e29dbed34f9bb6f510af1ee467aa3136be
EXTRADEEN <dinumira@gmail.com>
2023-03-19 17:43:19 +0100

    PEP8 validation fixed

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -17,17 +17,17 @@ if __name__ == '__main__':
 
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    
+
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
     state = session.query(State).order_by(State.id)\
         .filter(State.name == sys.argv[4]).first()
-    
+
     if not state:
         print("Not found")
     else:
         print("{}".format(state.id))
-        
+
     session.close()
