b4745890dcc9ff2983f85a0e198fe5c1baf09894
EXTRADEEN <dinumira@gmail.com>
2023-04-11 17:18:32 +0200

    case output fixed

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,17 +1,29 @@
 #!/usr/bin/node
 
-function second_biggest (arr) {
+function findSecondLargestElem (arr) {
   let first = -1; let second = -1;
 
   for (let i = 0; i <= arr.length - 1; i++) {
     if (arr[i] > first) {
       second = first;
       first = arr[i];
-    } else if (arr[i] > second && arr[i] != first) {
+    } else if (arr[i] > second && arr[i] !== first) {
       second = arr[i];
     }
   }
   console.log(second);
 }
-const arr = process.argv;
-second_biggest(arr);
+const array = process.argv;
+let idx = 2;
+const tab = [];
+while (idx in array) {
+  tab.push(+array[idx]);
+  idx++;
+}
+if (idx === 2) {
+  console.log(0);
+} else if (idx === 3) {
+  console.log(0);
+} else {
+  findSecondLargestElem(tab);
+}
