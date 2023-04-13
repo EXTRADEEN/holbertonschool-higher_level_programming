ee06e4f24ff8ffb60fa09eff18348c60ecbf6638
EXTRADEEN <dinumira@gmail.com>
2023-04-13 18:06:50 +0200

    script that creates a class with constructor that take 2 arguments

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// Defines a new Rectangle class that take 2 arguments
+
+class Rectangle {
+    constructor (w, h) {
+        this.width = w;
+        this.height = h;
+    }
+}
+
+module.exports = Rectangle;
