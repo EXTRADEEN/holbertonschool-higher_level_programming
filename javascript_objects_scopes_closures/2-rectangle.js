e6ecd3544a7e57571110d3d975cd7e16da4b02f7
EXTRADEEN <dinumira@gmail.com>
2023-04-13 18:16:26 +0200

    constructor that take pozitive arguments

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+// Defines a new Rectangle class that take 2 arguments
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
