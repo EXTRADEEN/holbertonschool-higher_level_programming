815e11c0b79b834d0d2278ad4df3109c6eff2612
EXTRADEEN <dinumira@gmail.com>
2023-04-17 19:12:58 +0200

    script that searchs by given ID

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+// script that prints the title of a Star Wars movie where the episode number matches a given integer
+
+const request = require('request');
+const objID = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${objID}`;
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  console.log(body.title);
+});
