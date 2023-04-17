3a07cf833941a41850816ee603b718c7f2aa87db
EXTRADEEN <dinumira@gmail.com>
2023-04-17 16:36:13 +0200

    script that writes a string to a file

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// script that writes a string to a file.
+
+const fs = require('fs');
+const file = process.argv[2];
+let fInput = process.argv[3];
+
+fs.writeFile(file, fInput, 'utf8' , (err) => {
+    if (err) throw (err);
+});
